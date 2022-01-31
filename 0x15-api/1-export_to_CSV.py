3b9464443b36cd694f5cfe2985a7a35897581912
Daniel-bee <daniel123_86@live.com>
2022-01-31 18:32:18 +0300

    export

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,14 +13,12 @@ if __name__ == "__main__":
     user_res = requests.get('{}/users/{}'.format(url, sys.argv[1])).json()
     todo_res = requests.get('{}/todos?userId={}'.format(
         url, sys.argv[1])).json()
-    user = user_res.get('username')
+
     with open('{}.csv'.format(sys.argv[1]), 'w', newline='') as csvfile:
         """
             export csv file userId.csv
         """
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for row in todo_res:
-            writer.writerow([sys.argv[1],
-                            user,
-                            row.get('completed'),
-                            row.get('title')])
+            writer.writerow([sys.argv[1], user_res.get('username'),
+                            row.get('completed'), row.get('title')])
