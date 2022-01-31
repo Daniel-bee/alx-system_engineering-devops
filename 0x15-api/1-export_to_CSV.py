e7754c9f7c71498d4580dd0ad6abfcf1d75eee49
Daniel-bee <daniel123_86@live.com>
2022-01-31 18:14:50 +0300

    export csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,9 +15,7 @@ if __name__ == "__main__":
         url, sys.argv[1])).json()
 
     with open('{}.csv'.format(sys.argv[1]), 'w', newline='') as csvfile:
-
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-
         for row in todo_res:
             writer.writerow([sys.argv[1],
                             user_res.get('username'),
