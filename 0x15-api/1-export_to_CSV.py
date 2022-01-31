23a65f1a8f78c6429bd31160e136b013f77f12e4
Daniel-bee <daniel123_86@live.com>
2022-01-31 17:23:38 +0300

    csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    script that, using this REST API, for a given employee
+    ID, returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com'
+    user_res = requests.get('{}/users/{}'.format(url, sys.argv[1])).json()
+    todo_res = requests.get('{}/todos?userId={}'.format(
+        url, sys.argv[1])).json()
+
+    with open(f'{sys.argv[1]}.csv', 'w', newline='') as csvfile:
+        fieldnames = ["userId", "id", "title", "completed"]
+
+        writer = csv.DictWriter(
+                csvfile, quoting=csv.QUOTE_ALL, fieldnames=fieldnames)
+
+        for row in todo_res:
+            row['id'] = user_res.get('name')
+            writer.writerow(row)
