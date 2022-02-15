0397360c75799ef6a4301291b7382ff8bb3a2892
Daniel-bee <daniel123_86@live.com>
2022-02-15 13:43:28 +0300

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
         If not a valid subreddit, return 0
     """
     res = requests.get('https://reddit.com/r/{}/about.json'.format(subreddit))
-    if res:
-        return res.json()['data']['subscribers']
-    else:
+    if not res:
         return 0
+
+    return res.json()['data']['subscribers']
