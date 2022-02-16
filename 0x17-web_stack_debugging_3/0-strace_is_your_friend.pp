16838128cf5327edde4c89d81f667599336ae06c
Daniel-bee <daniel123_86@live.com>
2022-02-16 23:11:06 +0300

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
-#Apache is returning a 500 error. Once you find the issue,
-#fix it and then automate it using Puppet (instead of using Bash as you were previously doing)
+#Apache is returning a 500 error. Once you find the issue
 exec { 'killmenow':
     command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
     path    => '/bin',
